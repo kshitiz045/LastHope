5e9196308915653908b0b0c8f1d98b3c89f996c7
kshitiz045 <kshitizg08@gmail.com>
2020-11-24 19:50:39 +0530

    new updates

--- php/home.php
+++ php/home.php
@@ -8,13 +8,81 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 <html>
 <head>
 	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style1.css">
 </head>
 <body>
-     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+     <h1>Hello , my friend<?php echo $_SESSION['user_name']; ?></h1>
+     <a href="../index.html"><h4>Click here to proceed</h4></a>
+    <a href="logout.php"><h4>Logout</h4></a>
+
+    <time id="time"></time>
+    <h1 id="message"></h1>
+    <h2 id="name" contenteditable="true"></h2>
+    <h3 id="day"></h3>
     
-     <a href="logout.php">Logout</a>
-     <a href="../index.html">Here</a>
+    
+    
+    <script>
+        const time = document.getElementById("time");
+        const message = document.getElementById("message");
+        const name = document.getElementById("name");
+        const day = document.getElementById("day");
+
+
+        name.addEventListener("keypress", setName);
+        name.addEventListener("blur", setName);
+
+
+        function showtime() {
+            let dateToday = new Date();
+            let hour = dateToday.getHours();
+            let minute = dateToday.getMinutes();
+            let seconds = dateToday.getSeconds();
+            let date = dateToday.toDateString();
+
+            const ampm = hour >= 12 ? 'P.M.' : 'A.M.';
+
+            hour = hour % 12 || 12;
+
+            hour = addZero(hour);
+            minute = addZero(minute);
+            seconds = addZero(seconds);
+
+
+            time.innerHTML = `${hour}<span>:</span>${minute}<span>:</span>${seconds} ${ampm}`;
+            day.innerHTML = `${date}`;
+            setTimeout(showtime, 1000);
+        }
+
+        function addZero(n) {
+            return (n < 10 ? '0' : '') + n;
+        }
+
+        function setmessage() {
+            let dateToday = new Date();
+            let hour = dateToday.getHours();
+
+            if (hour < 12) {
+                document.body.style.backgroundImage = 'url(https://images.pexels.com/photos/733100/pexels-photo-733100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)';
+                message.innerHTML = "Good Morning";
+            }
+            else if (hour < 18) {
+                document.body.style.backgroundImage = 'url(https://images.pexels.com/photos/316820/pexels-photo-316820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)';
+                message.innerHTML = "Good Afternoon";
+            }
+            else {
+                document.body.style.backgroundImage = 'url(https://images.pexels.com/photos/3083808/pexels-photo-3083808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)';
+                message.innerHTML = "Good Evening";
+                document.body.style.color = "white";
+            }
+        }
+
+        
+        showtime();
+        setmessage();
+       
+    </script>
+    
 </body>
 </html>
 
